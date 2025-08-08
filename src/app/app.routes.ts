e47eeae6cc2a131d4fd8d2d845650da964bb035b
Your Name <your.email@example.com>
2025-08-08 17:15:33 +0530

    add admin panel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AuthPage } from './pages/auth-page/auth-page';
 import { IntroPageComponent } from './pages/intro-page/intro-page';
 import { AuthGuard } from './auth.guard';
 import { CategoryDetailComponent } from './pages/category-detail-component/category-detail-component';
+import { AdminDashboardComponent } from './admin/admin-dashboard-component/admin-dashboard-component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
@@ -17,4 +18,19 @@ export const routes: Routes = [
   path: 'category/:id/details',
   component: CategoryDetailComponent
 }
+,
+
+// add by me 
+
+  {
+    path: 'home',
+    component: IntroPageComponent,
+    canActivate: [AuthGuard]
+  },
+
+  {
+    path: 'admin',
+    component: AdminDashboardComponent,
+    canActivate: [AuthGuard]
+  },
 ];
